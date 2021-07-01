156eddc03e12a997d04223f90cfd9174931460e8
RAVIKIRAN <rvkrn@gmail.com>
2021-07-01 07:57:56 +0530

    First Program

---
+++ CoreJava/src/com/basic/MyfirstProgram.java
@@ -0,0 +1,11 @@
+package com.basic;
+
+public class MyfirstProgram {
+
+	
+	public static void main(String[] args) {
+		//my frist application 
+		
+		System.out.println("My first Application");
+	}
+}
